6e2e1e2dc1fb52f524b762153a55c43eae863e31
Andrii Konovalov <78738364+andysmokk@users.noreply.github.com>
2021-10-13 21:32:51 +0200

    Updates

--- static/js/main.97868046.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-03-image-finder"]=this["webpackJsonpgoit-react-hw-03-image-finder"]||[]).push([[0],{10:function(e,t,n){"use strict";n.r(t);var i=n(1),r=n.n(i),c=n(3),o=n.n(c),a=(n(8),n(0));var d=function(){return Object(a.jsx)("div",{children:"Hola Mundo"})};o.a.render(Object(a.jsx)(r.a.StrictMode,{children:Object(a.jsx)(d,{})}),document.getElementById("root"))},8:function(e,t,n){}},[[10,1,2]]]);
-//# sourceMappingURL=main.97868046.chunk.js.map
