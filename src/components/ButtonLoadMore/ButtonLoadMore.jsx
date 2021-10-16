8025e043d3b898a508ecd63c69838641e8f02f9c
Andrii Konovalov <78738364+andysmokk@users.noreply.github.com>
2021-10-16 17:04:34 +0200

    added state machine

--- src/components/ButtonLoadMore/ButtonLoadMore.jsx
+++ src/components/ButtonLoadMore/ButtonLoadMore.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { toast } from 'react-toastify';
 import PropTypes from 'prop-types';
 import s from './ButtonLoadMore.module.css';
 import imagesAPI from '../../services/imagesApi';
@@ -31,7 +32,13 @@ class ButtonLoadMore extends Component {
     setTimeout(() => {
       imagesAPI
         .fetchImages(imageName, page + 1)
-        .then(images => loadMore(images.hits))
+        .then(images => {
+          images.hits.length === 0
+            ? toast.info(
+                `Картинки с названием ${`"${imageName}"`} закончились. Введите новое название.`,
+              )
+            : loadMore(images.hits);
+        })
         .then(this.setState({ page: page + 1 }))
         .catch(error => console.log(error))
         .finally(() => this.setState({ loading: false }));
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -9,8 +9,8 @@ import LoaderSpinner from '../LoaderSpinner/LoaderSpinner';
 class ImageGallery extends Component {
   state = {
     images: [],
-    loading: false,
     page: 1,
+    status: 'idle',
   };
 
   componentDidUpdate(prevProps, prevState) {
@@ -18,7 +18,7 @@ class ImageGallery extends Component {
     const { page, images } = this.state;
 
     if (prevProps.imageName !== imageName) {
-      this.setState({ loading: true });
+      this.setState({ status: 'pending' });
 
       setTimeout(() => {
         imagesAPI
@@ -28,10 +28,9 @@ class ImageGallery extends Component {
               ? toast.info(
                   `Картинок с названием ${`"${imageName}"`} нет. Введите новое название.`,
                 )
-              : this.setState({ images: images.hits });
+              : this.setState({ images: images.hits, status: 'resolved' });
           })
-          .catch(error => console.log(error))
-          .finally(() => this.setState({ loading: false }));
+          .catch(error => console.log(error));
       }, 1000);
     }
 
@@ -51,26 +50,51 @@ class ImageGallery extends Component {
 
   render() {
     const { state, loadMore } = this;
-    const { images, loading } = state;
+    const { images, status } = state;
     const { imageName, onOpenModal } = this.props;
 
-    return (
-      <>
-        {!imageName && <h2 className={s.title}>Введите название картинки</h2>}
-        <ul className={s.ImageGallery} onClick={onOpenModal}>
-          <ImageGalleryItem images={images} />
-        </ul>
-        {images.length > 0 && (
-          <ButtonLoadMore
-            images={images}
-            imageName={imageName}
-            loadMore={loadMore}
-          />
-        )}
-        {loading && <LoaderSpinner />}
-      </>
-    );
+    if (status === 'idle') {
+      return <h2 className={s.title}>Введите название картинки</h2>;
+    }
+
+    if (status === 'pending') {
+      return <LoaderSpinner />;
+    }
+
+    if (status === 'resolved') {
+      return (
+        <>
+          <ul className={s.ImageGallery} onClick={onOpenModal}>
+            <ImageGalleryItem images={images} />
+          </ul>
+          {images.length > 0 && (
+            <ButtonLoadMore
+              images={images}
+              imageName={imageName}
+              loadMore={loadMore}
+            />
+          )}
+        </>
+      );
+    }
   }
 }
 
 export default ImageGallery;
+
+// return (
+//   <>
+//     {!imageName && <h2 className={s.title}>Введите название картинки</h2>}
+//     <ul className={s.ImageGallery} onClick={onOpenModal}>
+//       <ImageGalleryItem images={images} />
+//     </ul>
+//     {images.length > 0 && (
+//       <ButtonLoadMore
+//         images={images}
+//         imageName={imageName}
+//         loadMore={loadMore}
+//       />
+//     )}
+//     {loading && <LoaderSpinner />}
+//   </>
+// );
